28774b8481ede0363a4d030e37e937e988c8792c
Herysson Rodrigues Figueiredo <heryssonf@gmail.com>
2024-05-20 10:59:42 -0300

    Delete CRUD directory

--- CRUD/src/main/java/com/example/CRUD/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CRUD.repository;
-
-import com.example.CRUD.model.Pessoa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PessoaRepository extends CrudRepository<Pessoa,Integer> {
-
-}
